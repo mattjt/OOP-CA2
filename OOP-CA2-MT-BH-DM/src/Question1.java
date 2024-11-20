a0b482e5a7d48a4663ec23c59ed4ae9168b8b6ea
mattjt <mattjpt@gmail.com>
2024-11-20 12:56:19 +0000

    Update Question1.java

--- OOP-CA2-MT-BH-DM/src/Question1.java
+++ OOP-CA2-MT-BH-DM/src/Question1.java
@@ -5,7 +5,7 @@
 public class Question1 {    // Interfaces
     public static void main(String[] args) {
         System.out.println("Question 1");
-
+        System.out.println("Test");
         // create container here...
     }
 }
