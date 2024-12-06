9915132fd491949573d117a63825fbcd42197165
mattjt <mattjpt@gmail.com>
2024-12-06 16:40:09 +0000

    Q6: added basic menu

--- OOP-CA2-MT-BH-DM/src/Question6.java
+++ OOP-CA2-MT-BH-DM/src/Question6.java
@@ -1,8 +1,10 @@
 package org.example;
 
+import java.util.Scanner;
+
 /**
- *  Name:
- *  Class Group:
+ *  Name: Matthew Tomkins
+ *  Class Group: SD2b
  */
 
 public class Question6      // Flight take-off (Queue)
@@ -10,6 +12,33 @@ public class Question6      // Flight take-off (Queue)
 
     public static void main(String[] args)
     {
+        System.out.println("Question 6\n__________\n");
+        System.out.println("Commands:\n'takeoff' followed by flightcode\n'land' followed by flightcode\n'next'\n'quit'\n");
+        Scanner scan = new Scanner(System.in);
+        while(true){
+            System.out.print("Enter command (takeoff, land, next, quit):");
+            String command = scan.nextLine().trim();
 
+            if (command.startsWith("takeoff")) {
+
+            }
+            else if (command.startsWith("land")) {
+
+            }
+            else if (command.equals("next")) {
+
+            }
+
+            else if (command.equals("quit")) {
+                System.out.println("Exiting program...");
+                break;
+            }
+            else {
+                System.out.println("Invalid command, enter(takeoff, land, next, or quit)");
+            }
+
+
+
+        }
     }
 }
